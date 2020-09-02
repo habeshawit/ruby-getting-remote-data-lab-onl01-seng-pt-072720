c9c5e89c4869d9f064ddfc68fd6160ff3450e033
Shalom Kebede <shalomseleshi@gmail.com>
2020-09-02 04:39:57 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -10,12 +10,9 @@ class GetRequester
   end
   
   def get_response_body 
-    # uri = URI.parse(url)
-    # response = Net::HTTP.get_response(uri)
-    # response.body 
-    uri = URI.parse(URL)
+    uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
-    response.body
+    response.body 
   end
   
   def parse_json 
